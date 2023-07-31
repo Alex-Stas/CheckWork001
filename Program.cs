c7510549a8e412e12afc586eaa8a30b33568c807
Aleksandr <alexander.stashchuk@gmail.com>
2023-07-31 11:30:54 +0900

    Final add read.me & diagrams

--- Program.cs
+++ Program.cs
@@ -14,72 +14,67 @@ string[] CreateArray(int size)
     return new string[size];
 }
 
-void FillRandomArray(string[] array, int lenght)
+void FillRandomArray(string[] array, int length)
 {
-    
     Random rnd = new Random();
-    char[] alphabet = {'A', 'B', 'C','D', 'E', 'F', 'G', 'H', 'I', 'J','K', 'L', 'M', 'N', 'O','P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}; 
+    char[] alphabet = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
     int alphabetLength = alphabet.Length;
+    
     for (int i = 0; i < array.Length; i++)
     {
-        
-        int wordLength = rnd.Next(1, lenght + 1);
-        array[i] = String.Empty; 
+        int wordLength = rnd.Next(1, length + 1);
+        array[i] = String.Empty;
         for (int j = 1; j <= wordLength; j++)
         {
             int letter = rnd.Next(0, alphabetLength);
             array[i] = array[i] + alphabet[letter];
         }
-            
     }
 }
-void PrintArray(string[] array)
+
+void PrintArray(string[] array, string text)
 {
+    Console.WriteLine();
+    Console.WriteLine(text);
+    
     for (int i = 0; i < array.Length; i++)
     {
         Console.WriteLine($"{array[i]}");
-    }
-    Console.WriteLine();
+    }  
 }
 
 string[] Get3Array(string[] array)
 {
-    int count3  = 0;
+    int count3 = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i].Length <= 3) count3++; 
+        if (array[i].Length <= 3) count3++;
     }
 
-    string[] resultArray = new string [count3];
+    string[] resultArray = new string[count3];
     int j = 0;
 
     for (int i = 0; i < array.Length; i++)
     {
-        
         if (array[i].Length <= 3)
         {
             resultArray[j] = array[i];
             j++;
-        }  
+        }
     }
 
     return resultArray;
-
 }
 
 
-
-
-
 int arraySize = InputNum("Введите размер массива (количество строк): ");
-int wordLength = InputNum("Введите максимальную длинну слова: ");
-string[] myArray = CreateArray(arraySize);
+int wordLength = InputNum("Введите максимальную длинну строки: ");
 
+string[] myArray = CreateArray(arraySize);
 FillRandomArray(myArray, wordLength);
-PrintArray(myArray);
+PrintArray(myArray, "Исходный массив:");
 
 string[] resultArray = Get3Array(myArray);
-
-PrintArray(resultArray);
+PrintArray(resultArray, "Результирующий массив:");
 
 
